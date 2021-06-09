8c119ae47488615b3111125475b650bd6367d339
Sebastion De Beer <sebastiandb@morecorp.co.za>
2021-06-09 12:53:17 +0200

    initial commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        // .net recognises Id as the primary key automatically to set up our databse and increment
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
